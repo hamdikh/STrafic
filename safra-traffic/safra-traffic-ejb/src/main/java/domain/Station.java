96bbaf754b84ffcf55d27aef78100f796ce11290
Montassar <Montassar@Monta>
2015-05-22 00:53:00 +0100

    Signed-off-by: Montassar <Montassar@Monta>

--- safra-traffic/safra-traffic-ejb/src/main/java/domain/Station.java
+++ safra-traffic/safra-traffic-ejb/src/main/java/domain/Station.java
@@ -10,6 +10,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import org.codehaus.jackson.annotate.JsonIgnore;
+
 /**
  * Entity implementation class for Entity: Station
  *
@@ -19,6 +21,9 @@ public class Station implements Serializable {
 
 	private Integer id;
 	private String name;
+	private String latitude;
+	private String longitude;
+
 	private static final long serialVersionUID = 1L;
 
 	private List<Stop> stops;
@@ -28,9 +33,11 @@ public class Station implements Serializable {
 		super();
 	}
 
-	public Station(String name) {
+	public Station(String name, String latitude, String longitude) {
 		super();
 		this.name = name;
+		this.latitude = latitude;
+		this.longitude = longitude;
 	}
 
 	@Id
@@ -52,7 +59,24 @@ public class Station implements Serializable {
 		this.name = name;
 	}
 
+	public String getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(String latitude) {
+		this.latitude = latitude;
+	}
+
+	public String getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(String longitude) {
+		this.longitude = longitude;
+	}
+
 	@OneToMany(mappedBy = "station")
+	@JsonIgnore
 	public List<Stop> getStops() {
 		return stops;
 	}
@@ -62,12 +86,43 @@ public class Station implements Serializable {
 	}
 
 	@OneToMany(mappedBy = "station")
+	@JsonIgnore
 	public List<Type> getTypes() {
 		return types;
 	}
 
 	public void setTypes(List<Type> types) {
 		this.types = types;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Station other = (Station) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Station [id=" + id + ", name=" + name + "]";
 	}
 
 }
--- safra-traffic/safra-traffic-ejb/src/main/java/services/utilities/PopulateDataBase.java
+++ safra-traffic/safra-traffic-ejb/src/main/java/services/utilities/PopulateDataBase.java
@@ -42,107 +42,107 @@ public class PopulateDataBase {
 
 	@PostConstruct
 	public void populateDb() {
-		Bus bus1 = new Bus("Bus01");
-		Bus bus2 = new Bus("Bus02");
-		Bus bus3 = new Bus("Bus03");
-		Bus bus4 = new Bus("Bus04");
-
-		Line line1 = new Line("10 Décembre - Ariana El Soghra");
-		Line line2 = new Line("Passage - Ariana");
-
-		Station station10 = new Station("10 Décembre");
-		Station station11 = new Station("Magasin Magro");
-		Station station12 = new Station("Enkhilette");
-		Station station13 = new Station("Pole Technologique El Ghazela");
-		Station station14 = new Station("Ariana El Soghra");
-
-		Station station21 = new Station("Passage");
-		Station station22 = new Station("Cité el Khathra");
-		Station station23 = new Station("Lycée Technique Arina");
-		Station station24 = new Station("Ariana");
-
-		List<Bus> listBuses1 = new ArrayList<>();
-		List<Bus> listBuses2 = new ArrayList<>();
-
-		listBuses1.add(bus1);
-		listBuses1.add(bus2);
-
-		listBuses2.add(bus3);
-		listBuses2.add(bus4);
-
-		line1.linkBusesToThisLine(listBuses1);
-		line2.linkBusesToThisLine(listBuses2);
-
-		Driver driver1 = new Driver(1, "Matin", "Mohamed", "med@driver.tn",
-				"0000");
-		entityManager.persist(driver1);
-
-		Driver driver2 = new Driver(1, "Matin", "Saleh", "saleh@driver.tn",
-				"0000");
-		entityManager.persist(driver2);
-
-		Driver driver3 = new Driver(1, "Aprés midi", "Nidhal",
-				"nidhal@driver.tn", "0000");
-		entityManager.persist(driver3);
-
-		Driver driver4 = new Driver(1, "Aprés midi", "Samir",
-				"samir@driver.tn", "0000");
-		entityManager.persist(driver4);
-
-		BusMan busMan = new BusMan(5, "Seif", "seif@man.tn", "0000");
-		entityManager.persist(busMan);
-
-		BusMan busMan2 = new BusMan(5, "Amine", "amine@man.tn", "0000");
-		entityManager.persist(busMan2);
-
-		try {
-			Passenger passenger = new Passenger((Double) 120.0,
-					new SimpleDateFormat("MM/dd/yyyy").parse("11/25/1991"),
-					true, "Patricia", "patricia@passenger.tn", "0000");
-			entityManager.persist(passenger);
-		} catch (Exception e) {
-		}
-
-		// Creation of a line
-
-		Map<Integer, Station> stations1 = new HashMap();
-		Map<Integer, Station> stations2 = new HashMap();
-
-		stations1.put(0, station10);
-		stations1.put(1, station11);
-		stations1.put(2, station12);
-		stations1.put(2, station13);
-		stations1.put(2, station14);
-
-		stations2.put(0, station21);
-		stations2.put(1, station22);
-		stations2.put(2, station23);
-		stations2.put(1, station24);
-
-		entityManager.persist(line1);
-		entityManager.persist(line2);
-
-		entityManager.persist(station10);
-		entityManager.persist(station11);
-		entityManager.persist(station12);
-		entityManager.persist(station13);
-		entityManager.persist(station14);
-		entityManager.persist(station21);
-		entityManager.persist(station22);
-		entityManager.persist(station23);
-		entityManager.persist(station24);
-
-		entityManager.persist(bus1);
-		entityManager.persist(bus2);
-		entityManager.persist(bus3);
-		entityManager.persist(bus4);
-
-		stationServicesLocal.createLine(line1, stations1);
-
-		Stop stop1 = new Stop(0, stationServicesLocal.findBusByName("Bus01"),
-				stationServicesLocal.findStationByName("10 Décembre"));
-
-		entityManager.persist(stop1);
+//		Bus bus1 = new Bus("Bus01");
+//		Bus bus2 = new Bus("Bus02");
+//		Bus bus3 = new Bus("Bus03");
+//		Bus bus4 = new Bus("Bus04");
+//
+//		Line line1 = new Line("10 Décembre - Ariana El Soghra");
+//		Line line2 = new Line("Passage - Ariana");
+//
+//		Station station10 = new Station("10 Décembre");
+//		Station station11 = new Station("Magasin Magro");
+//		Station station12 = new Station("Enkhilette");
+//		Station station13 = new Station("Pole Technologique El Ghazela");
+//		Station station14 = new Station("Ariana El Soghra");
+//
+//		Station station21 = new Station("Passage");
+//		Station station22 = new Station("Cité el Khathra");
+//		Station station23 = new Station("Lycée Technique Arina");
+//		Station station24 = new Station("Ariana");
+//
+//		List<Bus> listBuses1 = new ArrayList<>();
+//		List<Bus> listBuses2 = new ArrayList<>();
+//
+//		listBuses1.add(bus1);
+//		listBuses1.add(bus2);
+//
+//		listBuses2.add(bus3);
+//		listBuses2.add(bus4);
+//
+//		line1.linkBusesToThisLine(listBuses1);
+//		line2.linkBusesToThisLine(listBuses2);
+//
+//		Driver driver1 = new Driver(1, "Matin", "Mohamed", "med@driver.tn",
+//				"0000");
+//		entityManager.persist(driver1);
+//
+//		Driver driver2 = new Driver(1, "Matin", "Saleh", "saleh@driver.tn",
+//				"0000");
+//		entityManager.persist(driver2);
+//
+//		Driver driver3 = new Driver(1, "Aprés midi", "Nidhal",
+//				"nidhal@driver.tn", "0000");
+//		entityManager.persist(driver3);
+//
+//		Driver driver4 = new Driver(1, "Aprés midi", "Samir",
+//				"samir@driver.tn", "0000");
+//		entityManager.persist(driver4);
+//
+//		BusMan busMan = new BusMan(5, "Seif", "seif@man.tn", "0000");
+//		entityManager.persist(busMan);
+//
+//		BusMan busMan2 = new BusMan(5, "Amine", "amine@man.tn", "0000");
+//		entityManager.persist(busMan2);
+//
+//		try {
+//			Passenger passenger = new Passenger((Double) 120.0,
+//					new SimpleDateFormat("MM/dd/yyyy").parse("11/25/1991"),
+//					true, "Patricia", "patricia@passenger.tn", "0000");
+//			entityManager.persist(passenger);
+//		} catch (Exception e) {
+//		}
+//
+//		// Creation of a line
+//
+//		Map<Integer, Station> stations1 = new HashMap();
+//		Map<Integer, Station> stations2 = new HashMap();
+//
+//		stations1.put(0, station10);
+//		stations1.put(1, station11);
+//		stations1.put(2, station12);
+//		stations1.put(2, station13);
+//		stations1.put(2, station14);
+//
+//		stations2.put(0, station21);
+//		stations2.put(1, station22);
+//		stations2.put(2, station23);
+//		stations2.put(1, station24);
+//
+//		entityManager.persist(line1);
+//		entityManager.persist(line2);
+//
+//		entityManager.persist(station10);
+//		entityManager.persist(station11);
+//		entityManager.persist(station12);
+//		entityManager.persist(station13);
+//		entityManager.persist(station14);
+//		entityManager.persist(station21);
+//		entityManager.persist(station22);
+//		entityManager.persist(station23);
+//		entityManager.persist(station24);
+//
+//		entityManager.persist(bus1);
+//		entityManager.persist(bus2);
+//		entityManager.persist(bus3);
+//		entityManager.persist(bus4);
+//
+//		stationServicesLocal.createLine(line1, stations1);
+//
+//		Stop stop1 = new Stop(0, stationServicesLocal.findBusByName("Bus01"),
+//				stationServicesLocal.findStationByName("10 Décembre"));
+//
+//		entityManager.persist(stop1);
 
 	}
 
